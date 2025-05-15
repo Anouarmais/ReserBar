cd16d513233a21b6cba80e7d8debb4be889ac7b1
19JuanDavid <Juandavidquiroga7@gmail.com>
2025-05-15 03:14:29 +0200

    Se han añadido restaurantes nuevos sin duplicarse a la base de datos

--- app/Http/Controllers/SerpApiController.php
+++ app/Http/Controllers/SerpApiController.php
@@ -4,35 +4,69 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Restaurante;
 
 class SerpApiController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
-        $query = [
-            'engine' => 'google_maps',
-            'q' => 'Coffee',
-            'll' => '@40.7455096,-74.0083012,14z',
-            'api_key' => '562ec3be9eb9bf89c5608bb560351f60ed9fefc83cb6ec41ef985fcfeadc4c2a'
-
+        // Coordenadas aproximadas de las zonas
+        $zonas = [
+            'norte'  => '@38.031059,-1.130090,14z',
+            'centro' => '@37.986059,-1.130090,14z',
+            'sur'    => '@37.950059, -1.130090,14z',
+            'este'   => '@37.986059,-1.100090,14z',
+            'oeste'  => '@37.986059,-1.160090,14z',
         ];
 
-        // Realizar la solicitud a la API
-        $response = Http::get('https://serpapi.com/search.json', $query);
-
-        // Obtener los resultados locales
-        $results = $response->json()['local_results'] ?? [];
-
-        // Extraer solo los ratings de los resultados
-        $ratings = array_map(function($result) {
-            return [
-                'title' => $result['title'],
-                'rating' => $result['rating'] ?? 'No rating available',
+        $resultadosTotales = [];
+        foreach ($zonas as $zona => $coordenadas) {
+            $query = [
+                'engine' => 'google_maps',
+                'q' => 'restaurants',
+                'll' => $coordenadas,
+                'radius' => '5000',
+                'type' => 'search',
+                'api_key' => '7c846028bf251019d896fa58d69307ef055bc6e2ca91f45bbee7fff4364e80b5'
             ];
-        }, $results);
 
-        // Devolver los ratings en formato JSON
-        return response()->json($ratings);
+            $response = Http::get('https://serpapi.com/search.json', $query);
 
+
+            $results = $response->json()['local_results'] ?? [];
+
+
+            $DatosResturant = array_map(function ($result) use ($zona) {
+                $extensions = collect($result['extensions'] ?? []);
+                return [
+                    'nombre' => $result['title'] ?? null,
+                    'rating' => $result['rating'] ?? null,
+                    'precio' => $result['price'] ?? null,
+                    'tipo_comida' => $result['type'] ?? null,
+                    'direccion' => $result['address'] ?? null,
+                    'estado_apertura' => $result['open_state'] ?? null,
+                    'horarios' => $result['operating_hours'] ?? null,
+                    'telefono' => $result['phone'] ?? null,
+                    'sitio_web' => $result['website'] ?? null,
+                    'accesibilidad' => $extensions->firstWhere('accessibility')["accessibility"] ?? null,
+                    'que_ofrecen' => $extensions->firstWhere('offerings')["offerings"] ?? null,
+                    'popular_por' => $extensions->firstWhere('popular_for')["popular_for"] ?? null,
+                    'parking' => $extensions->firstWhere('parking')["parking"] ?? null,
+                    'amenidades' => $extensions->firstWhere('amenities')["amenities"] ?? null,
+                    'zona' => $zona,
+                    'clicks' => null
+                ];
+            }, $results);
+
+            foreach ($DatosResturant as $data) {
+                if (!Restaurante::where('nombre', $data['nombre'])->where('direccion', $data['direccion'])->exists()) {
+                    Restaurante::create($data);
+                }
+            }
+            $resultadosTotales = array_merge($resultadosTotales, $DatosResturant);
+        }
+
+        return response()->json($resultadosTotales);
     }
 }
