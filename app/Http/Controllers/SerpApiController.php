7d4aa7f22f81dc305e94b4d1d6f274a5f03d38b3
DESKTOP-184VAIE\ANOUAR <maisanouar30@gmail.com>
2025-05-29 13:02:22 +0200

    Modificación del controlador SerpApi

--- app/Http/Controllers/SerpApiController.php
+++ app/Http/Controllers/SerpApiController.php
@@ -1,38 +1,57 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
+use Illuminate\Http\Request;
+use App\Models\Restaurante;
 class SerpApiController extends Controller
 {
-    public function index() {
-
+    public function index()
+    {
+   
         $query = [
             'engine' => 'google_maps',
-            'q' => 'Coffee',
-            'll' => '@40.7455096,-74.0083012,14z',
-            'api_key' => '562ec3be9eb9bf89c5608bb560351f60ed9fefc83cb6ec41ef985fcfeadc4c2a'
-
+            'q' => 'restaurants',
+            'll' => '@37.983311,-1.128172,14z',
+            'api_key' => '7c846028bf251019d896fa58d69307ef055bc6e2ca91f45bbee7fff4364e80b5'
         ];
 
-        // Realizar la solicitud a la API
         $response = Http::get('https://serpapi.com/search.json', $query);
 
-        // Obtener los resultados locales
+
         $results = $response->json()['local_results'] ?? [];
 
-        // Extraer solo los ratings de los resultados
-        $ratings = array_map(function($result) {
+   
+        $DatosResturant = array_map(function($result) {
+            $extensions = collect($result['extensions'] ?? []);
             return [
-                'title' => $result['title'],
-                'rating' => $result['rating'] ?? 'No rating available',
+                'name' => $result['title'] ?? null,
+                'rating' => $result['rating'] ?? null,
+                'precio' => $result['price'] ?? null,
+                'tipo_comida' => $result['type'] ?? null,
+                'direccion' => $result['address'] ?? null,
+                'estado_apertura' => $result['open_state'] ?? null,
+                'horarios' => $result['operating_hours'] ?? null,
+                'telefono' => $result['phone'] ?? null,
+                'sitio_web' => $result['website'] ?? null,
+                'accesibilidad' => $extensions->firstWhere('accessibility')["accessibility"] ?? null,
+                'que_ofrecen' => $extensions->firstWhere('offerings')["offerings"] ?? null,
+                'popular_por' => $extensions->firstWhere('popular_for')["popular_for"] ?? null,
+                'parking' => $extensions->firstWhere('parking')["parking"] ?? null,
+                'amenidades' => $extensions->firstWhere('amenities')["amenities"] ?? null,
+                'clicks' => null 
             ];
         }, $results);
 
-        // Devolver los ratings en formato JSON
-        return response()->json($ratings);
+        foreach ($DatosResturant as $data) {
+          
+        
+       
+                Restaurante::create($data);
+          
+        }
+        
 
+
+        return response()->json($DatosResturant);
     }
 }
